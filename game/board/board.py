3c3fa84603b16666e59d1a86ee558138c7312156
Jau Tung Chan <chanjautung212@gmail.com>
2021-12-25 16:55:36 -0800

    Player abstraction

--- game/board/board.py
+++ game/board/board.py
@@ -2,6 +2,7 @@ from enum import Enum
 import logging
 from typing import SupportsIndex
 from game.board.minion import Minion
+from game.player.face import Face
 from utils.safe_list import SafeList
 
 class BoardSide(Enum):
@@ -9,14 +10,16 @@ class BoardSide(Enum):
     OPPONENT = 2
 
 class Board:
-    def __init__(self) -> None:
+    def __init__(self, player_face: Face, opponent_face: Face) -> None:
+        self._player_face = player_face
+        self._opponent_face = opponent_face
         self._player_minions: SafeList[Minion] = SafeList()
         self._opponent_minions: SafeList[Minion] = SafeList()
 
     def add_minion(self, minion: Minion, board_side: BoardSide) -> None:
         self._minions_on_side(board_side).append(minion)
 
-    def minion_attack(
+    def minion_attack_minion(
         self,
         attacker_index: SupportsIndex,
         target_index: SupportsIndex,
@@ -24,18 +27,28 @@ class Board:
     ) -> None:
         attacker = self._minions_on_side(attacker_side).get(attacker_index)
         target = self._minions_on_opposing_side(attacker_side).get(target_index)
-
         if attacker is None or target is None:
             logging.error("Either the attacker or target minion indices are out of range")
             return
 
         attacker.take_damage(target.get_attack())
         target.take_damage(attacker.get_attack())
-
         if attacker.is_dead():
             self._minions_on_side(attacker_side).pop(attacker_index)
         if target.is_dead():
             self._minions_on_opposing_side(attacker_side).pop(target_index)
+
+    def minion_attack_face(
+        self,
+        attacker_index: SupportsIndex,
+        attacker_side: BoardSide
+    ) -> None:
+        attacker = self._minions_on_side(attacker_side).get(attacker_index)
+        if attacker is None:
+            logging.error("The attacker minion index is out of range")
+            return
+
+        self._face_on_opposing_side(attacker_side).take_damage(attacker.get_attack())
 
     def switch_players(self) -> None:
         self._player_minions, self._opponent_minions = self._opponent_minions, self._player_minions
@@ -55,5 +68,23 @@ class Board:
                 return self._opponent_minions
             case BoardSide.OPPONENT:
                 return self._player_minions
+            case _:
+                logging.error("Board side of unknown type")
+
+    def _face_on_side(self, board_side: BoardSide) -> Face:
+        match board_side:
+            case BoardSide.PLAYER:
+                return self._player_face
+            case BoardSide.OPPONENT:
+                return self._opponent_face
+            case _:
+                logging.error("Board side of unknown type")
+
+    def _face_on_opposing_side(self, board_side: BoardSide) -> Face:
+        match board_side:
+            case BoardSide.PLAYER:
+                return self._opponent_face
+            case BoardSide.OPPONENT:
+                return self._player_face
             case _:
                 logging.error("Board side of unknown type")
---
+++ game/player/player.py
@@ -0,0 +1,38 @@
+import logging
+from game.cards.card import Card
+from game.cards.fatigue_card import FatigueCard
+from game.player.deck import Deck
+from game.player.face import Face
+from game.player.hand import Hand
+from game.player.mana_bin import ManaBin
+
+class Player:
+    def __init__(self, gid: str, deck: Deck) -> None:
+        self._gid = gid
+        self._deck = deck
+        self._deck.shuffle()
+        self._hand = Hand()
+        self._face = Face()
+        self._mana_bin = ManaBin()
+
+    def get_face(self) -> Face:
+        return self._face
+
+    def start_turn(self) -> None:
+        self._mana_bin.increment_mana()
+        drawn_object = self._deck.draw_top_card()
+        if isinstance(drawn_object, FatigueCard):
+            self._face.take_damage(drawn_object.get_fatigue_damage())
+            logging.debug("Player (%s) takes %s fatigue damage", self._gid, drawn_object.get_fatigue_damage())
+        elif isinstance(drawn_object, Card):
+            self._hand.add_card(drawn_object)
+            logging.debug("Player (%s) draws card '%s'", self._gid, drawn_object)
+        else:
+            logging.error("Drawn object of unknown type")
+
+    def __str__(self) -> str:
+        return f"Player {self._gid}: "\
+            f"Health ({self._face.get_current_health()}), "\
+            f"Hand ({self._hand.get_size()}), "\
+            f"Deck ({self._deck.get_size()}), "\
+            f"Mana ({self._mana_bin.get_mana()})"
